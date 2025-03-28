0f3ed7e21505bb978f3672da2756f9ed546df940
Martin Weitzel <github@tbfe.de>
2025-03-28 17:06:25 +0100

    added expectations for SBstrlen to test

--- OOP-in-C/v13/sbuf_Test.c
+++ OOP-in-C/v13/sbuf_Test.c
@@ -2,24 +2,40 @@
 
 #include "SBuffer.h"
 
+#include <string.h> /* memcmp */
+
 static char largebuffer[1000];
 static char smallbuffer[10];
 
 TEST(sbuf, large) {
     int nchars;
     SBuffer sbuf = SBuffer_INIT(largebuffer);
+    EXPECT_EQ(0, SBstrlen(&sbuf));
+
     nchars = SBprintf(&sbuf, "hello\n");
     EXPECT_EQ(6, nchars);
     ASSERT_STREQ("hello\n", sbuf.static_buffer);
+    EXPECT_EQ(6, SBstrlen(&sbuf));
+
     nchars = SBprintf(&sbuf, "world\n");
     EXPECT_EQ(6, nchars);
     ASSERT_STREQ("hello\n"
                  "world\n", sbuf.static_buffer);
+    EXPECT_EQ(12, SBstrlen(&sbuf));
+
     nchars = SBprintf(&sbuf, "-----");
     EXPECT_EQ(5, nchars);
     ASSERT_STREQ("hello\n"
                  "world\n"
                  "-----", sbuf.static_buffer);
+    EXPECT_EQ(17, SBstrlen(&sbuf));
+
+    static const char nulled[sizeof largebuffer];
+    SBclear(&sbuf);
+    EXPECT_EQ(0, SBstrlen(&sbuf));
+    EXPECT_FALSE(memcmp(sbuf.static_buffer, nulled, sizeof nulled) == 0);
+    SBpurge(&sbuf);
+    EXPECT_TRUE(memcmp(sbuf.static_buffer, nulled, sizeof nulled) == 0);
 }
 
 TEST(sbuf, small) {
@@ -31,9 +47,9 @@ TEST(sbuf, small) {
     nchars = SBprintf(&sbuf, "world\n");
     EXPECT_EQ(6, nchars);
     ASSERT_STREQ("hello\n"
-                 "wo", sbuf.static_buffer);
+                 "wor", sbuf.static_buffer);
     nchars = SBprintf(&sbuf, "-----");
     EXPECT_EQ(5, nchars);
     ASSERT_STREQ("hello\n"
-                 "wo", sbuf.static_buffer);
+                 "wor", sbuf.static_buffer);
 }
